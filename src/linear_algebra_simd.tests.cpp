37a19e48ca9f736a73701593524dfed64c7dc78a
Antony Peacock <ant.peacock@gmail.com>
2019-12-11 22:04:30 +0000

    Add project headers and test project

---
+++ src/linear_algebra_simd.tests.cpp
@@ -0,0 +1,27 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <linear_algebra_simd.hpp>
+
+/*! Helper function to capture constexpr results in catch test reporting.
+    \note
+        Credit to Jason Turner: https://twitter.com/lefticus/status/980530307580514304
+    \tparam B
+        Compile time condition.
+    \return
+        The compile time condition result.
+ */
+template<bool B>
+constexpr bool static_test()
+{
+    static_assert(B);
+    return B;
+}
+
+
+TEST_CASE("Test ABI type traits", "[simd.abi.traits]")
+{
+//    REQUIRE(static_test<is_abi_tag<simd_abi::scalar>::type::value>());
+//    REQUIRE(static_test<is_abi_tag<simd_abi::detail::sse_register>::type::value>());
+//    REQUIRE(static_test<is_abi_tag<simd_abi::detail::avx_register>::type::value>());
+//    REQUIRE(static_test<is_abi_tag<simd_abi::detail::avx512_register>::type::value>());
+}
